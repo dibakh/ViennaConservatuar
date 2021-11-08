a571e050d1e3f73a5986b83d412d51cd4b193fa2
Diba Kheirkhah <diba.kheirkhah@gmail.com>
2021-11-08 10:11:43 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vienna-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ViennaConservatuar';
+}
