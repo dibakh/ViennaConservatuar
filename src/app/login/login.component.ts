1409fb2e9a0d48c57c5ea4bbc8664157eb1c0774
Diba Kheirkhah <diba.kheirkhah@gmail.com>
2021-11-21 11:54:21 +0100

    create login component + update app.module

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vienna-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
