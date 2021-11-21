63215ebb8f09a325109b3d5845f33e83160cd821
Diba Kheirkhah <diba.kheirkhah@gmail.com>
2021-11-21 18:55:40 +0100

    implement register component + remove ngOnInit

--- src/app/registeration/registeration.component.ts
+++ src/app/registeration/registeration.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'vienna-registeration',
   templateUrl: './registeration.component.html',
   styleUrls: ['./registeration.component.scss']
 })
-export class RegisterationComponent implements OnInit {
+export class RegisterationComponent  {
 
   constructor() { }
 
-  ngOnInit(): void {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  hide = true;
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
   }
+
 
 }
